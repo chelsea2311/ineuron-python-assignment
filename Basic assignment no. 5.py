44b88634d7e58986c132c1959edeb4e567db7621
Chelsea Rodrigues <84131870+chelsea2311@users.noreply.github.com>
2021-08-27 12:22:06 +0530

    Add files via upload

---
+++ Basic assignment no. 5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+ASSIGNMENT NO 5
+
+
+# Q1. Empty dictionary code looks like : {}
+
+# Q2. The value of dictionary value with key 'foo' and value 42 is {'foo':42}
+
+# Q3.The most significant diffrence between dictionary and list is dictionaries are unordered sets and the items in the list are ordered . Also the main difference is that items in dictionaries are accessed via keys and not via their position. 
+
+# Q4. If spam={'bar':100} and if we tried to access 'foo' then we get an error.
+
+# Q5. If dictionary is stored in spam, there is no difference between the expressions'cat' in spam and 'cat' in spam.keys() both operations checks whether there is key as 'cat' in dictionary.
+
+# Q6.If a dictionary is stored in spam, 'cat' in spam checks whether there is a key as 'cat' in the dictionary, while 'cat' in spam.values() checks whether there is a value 'cat' for one of the keys in dictionary. 
+
+# Q7. Shortcut for the below given code is ,  spam.setdefault('color','black')
+
+# Q8. We "pretty print" dictionary values using pprint.pprint() module and function
